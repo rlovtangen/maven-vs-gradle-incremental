8f91d430b58750d7608610b39838d7928a895851
Ronny Løvtangen <ronny@lovtangen.com>
2016-03-12 15:39:53 +0100

    Initial project

---
+++ incremental-module1/src/main/java/com/lovtangen/Foo.java
@@ -0,0 +1,8 @@
+package com.lovtangen;
+
+public class Foo {
+  
+  public static String doSomething() {
+    return "foo";
+  }
+}
---
+++ incremental-module2/src/main/java/com/lovtangen/Bar.java
@@ -0,0 +1,8 @@
+package com.lovtangen;
+
+public class Bar {
+  
+  public void useFoo() {
+    Foo.doSomething();
+  }
+}
